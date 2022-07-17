f288dcfb6ffd3af8dc912ea9bcc50451bd9fc1a0
Francesco Parra <francescoparra92@gmail.com>
2022-07-17 19:58:39 +0200

    Only frontend

--- server/routes/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const userController = require("../controller/userController");
-
-const router = express.Router();
-
-router.post("/user/register", userController.newUser);
-
-module.exports = router;
